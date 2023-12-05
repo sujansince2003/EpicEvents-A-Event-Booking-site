1e687010090ae5ec94ae5e3b072655ae2f699436
sujan khatri <sujansince2059@gmail.com>
2023-12-05 14:38:45 +0545

    header styling completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <img className="logo" src="/assets/logo4.png" alt="" />
+      <nav>
+        <ul>
+          <li>
+            <a href="/#">Home</a>
+          </li>
+          <li>
+            <a href="/#">Concerts</a>
+          </li>
+          <li>
+            <a href="/#">Movies</a>
+          </li>
+          <li>
+            <a href="/#">Threater shows</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="auth-buttons">
+        <button className="btn btn-login">Login</button>
+        <button className="btn btn-register">Register</button>
+      </div>
+    </header>
   );
 }
 
