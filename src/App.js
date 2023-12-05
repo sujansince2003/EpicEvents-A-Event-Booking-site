1e1b80e0c71826c13365b72651f70f8be57c08c4
sujan khatri <sujansince2059@gmail.com>
2023-12-05 23:58:35 +0545

    event details

--- src/App.js
+++ src/App.js
@@ -26,17 +26,17 @@ function App() {
           <button className="btn btn-register">Register</button>
         </div>
       </header>
-      <section className="hero">
+      {/* <section className="hero">
         <div className="card">
           <div className="postercontainer">
             <img className="poster" src="/assets/poster.jpg" alt="" />
           </div>
           <div className="info">
-            <span className="title">Ultimate Avengers: The Movie</span>
+            <p className="title">Ultimate Avengers: The Movie</p>
             <div className="location-time">
-              <span>December 20,2023</span>
+              <p>December 20,2023</p>
               <img className="dot" src="/assets/dot.png" alt="" />
-              <span>Pokhara,Nepal</span>
+              <p>Pokhara,Nepal</p>
             </div>
           </div>
         </div>
@@ -116,6 +116,56 @@ function App() {
               <img className="dot" src="/assets/dot.png" alt="" />
               <span>Pokhara,Nepal</span>
             </div>
+          </div>
+        </div>
+      </section> */}
+      <section className="hero1">
+        <div className="event-detail ">
+          <h1>Event Details</h1>
+          <div className="event-info">
+            <div className="icon-container">
+              <img
+                className="event-info-icon"
+                src="assets/eventicon.png"
+                alt=""
+              />
+            </div>
+            <div className="info-text">
+              <p className="info-text-1">Date & Time</p>
+              <p className="info-text-2">Sat, Dec 30, 2023 11:30 AM</p>
+            </div>
+          </div>
+          <hr />
+          <div className="event-info">
+            <div className="icon-container">
+              <img
+                className="event-info-icon"
+                src="assets/locationicon.png"
+                alt=""
+              />
+            </div>
+            <div className="info-text">
+              <p className="info-text-1">Location</p>
+              <p className="info-text-2">Kathmandu,Nepal</p>
+              <a href="/#">view on map</a>
+            </div>
+          </div>
+          <hr />
+          <h3>Select Tickets</h3>
+          <div className="ticket">
+            <div className="ticket-n-counter">
+              <div className="ticket-info">
+                <span className="ticket-num">1x tickets</span>
+                <span className="ticket-price"> USD $500.00</span>
+              </div>
+              <div className="counter">
+                <button className="counter-btn btn-dec">-</button>
+                <span className="ticket-count">2</span>
+                <button className="counter-btn btn-inc">+</button>
+              </div>
+            </div>
+
+            <button className="checkout-btn">Checkout for $1000.00</button>
           </div>
         </div>
       </section>
