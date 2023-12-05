a07d4a5025921c3dc244e472b07ba2fd3076a366
sujan khatri <sujansince2059@gmail.com>
2023-12-05 21:40:47 +0545

    screen1

--- src/App.js
+++ src/App.js
@@ -2,29 +2,124 @@ import "./App.css";
 
 function App() {
   return (
-    <header>
-      <img className="logo" src="/assets/logo4.png" alt="" />
-      <nav>
-        <ul>
-          <li>
-            <a href="/#">Home</a>
-          </li>
-          <li>
-            <a href="/#">Concerts</a>
-          </li>
-          <li>
-            <a href="/#">Movies</a>
-          </li>
-          <li>
-            <a href="/#">Threater shows</a>
-          </li>
-        </ul>
-      </nav>
-      <div className="auth-buttons">
-        <button className="btn btn-login">Login</button>
-        <button className="btn btn-register">Register</button>
-      </div>
-    </header>
+    <>
+      <header>
+        <img className="logo" src="/assets/logo4.png" alt="" />
+        <nav>
+          <ul>
+            <li>
+              <a href="/#">Home</a>
+            </li>
+            <li>
+              <a href="/#">Concerts</a>
+            </li>
+            <li>
+              <a href="/#">Movies</a>
+            </li>
+            <li>
+              <a href="/#">Threater shows</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="auth-buttons">
+          <button className="btn btn-login">Login</button>
+          <button className="btn btn-register">Register</button>
+        </div>
+      </header>
+      <section className="hero">
+        <div className="card">
+          <div className="postercontainer">
+            <img className="poster" src="/assets/poster.jpg" alt="" />
+          </div>
+          <div className="info">
+            <span className="title">Ultimate Avengers: The Movie</span>
+            <div className="location-time">
+              <span>December 20,2023</span>
+              <img className="dot" src="/assets/dot.png" alt="" />
+              <span>Pokhara,Nepal</span>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="postercontainer">
+            <img className="poster" src="/assets/poster.jpg" alt="" />
+          </div>
+          <div className="info">
+            <span className="title">Ultimate Avengers: The Movie</span>
+            <div className="location-time">
+              <span>December 20,2023</span>
+              <img className="dot" src="/assets/dot.png" alt="" />
+              <span>Pokhara,Nepal</span>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="postercontainer">
+            <img className="poster" src="/assets/poster.jpg" alt="" />
+          </div>
+          <div className="info">
+            <span className="title">Ultimate Avengers: The Movie</span>
+            <div className="location-time">
+              <span>December 20,2023</span>
+              <img className="dot" src="/assets/dot.png" alt="" />
+              <span>Pokhara,Nepal</span>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="postercontainer">
+            <img className="poster" src="/assets/poster.jpg" alt="" />
+          </div>
+          <div className="info">
+            <span className="title">Ultimate Avengers: The Movie</span>
+            <div className="location-time">
+              <span>December 20,2023</span>
+              <img className="dot" src="/assets/dot.png" alt="" />
+              <span>Pokhara,Nepal</span>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="postercontainer">
+            <img className="poster" src="/assets/poster.jpg" alt="" />
+          </div>
+          <div className="info">
+            <span className="title">Ultimate Avengers: The Movie</span>
+            <div className="location-time">
+              <span>December 20,2023</span>
+              <img className="dot" src="/assets/dot.png" alt="" />
+              <span>Pokhara,Nepal</span>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="postercontainer">
+            <img className="poster" src="/assets/poster.jpg" alt="" />
+          </div>
+          <div className="info">
+            <span className="title">Ultimate Avengers: The Movie</span>
+            <div className="location-time">
+              <span>December 20,2023</span>
+              <img className="dot" src="/assets/dot.png" alt="" />
+              <span>Pokhara,Nepal</span>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="postercontainer">
+            <img className="poster" src="/assets/poster.jpg" alt="" />
+          </div>
+          <div className="info">
+            <span className="title">Ultimate Avengers: The Movie</span>
+            <div className="location-time">
+              <span>December 20,2023</span>
+              <img className="dot" src="/assets/dot.png" alt="" />
+              <span>Pokhara,Nepal</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
